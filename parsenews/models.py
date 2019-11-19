fdd38aa7fe354162fe38e784609f83879489894f
acquaint <email4thebest@gmail.com>
2019-11-19 17:01:09 +0530

    Little Bit Perfect

--- parsenews/models.py
+++ parsenews/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.TextField()
+    link = models.TextField()
+    nid = models.TextField()
+    published = models.TextField()
+    summary = models.TextField()
+
+ 
+    def __str__(self):
+        return self.title
+
--- parsenews/views.py
+++ parsenews/views.py
@@ -1,5 +1,52 @@
 from django.shortcuts import render
+import feedparser as fp
+import json
+from time import mktime
+from datetime import datetime
+import requests 
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request,'newsbox/index.html')
+    
+    URL = 'https://news.google.com/rss/search?pz=1&cf=all&q=nse$&hl=en-IN&gl=IN&ceid=IN:en' 
+
+    feed = fp.parse(URL)
+    newsdb = News.objects.all()
+
+    viewdict = dict()
+    for post in feed.entries:
+        # print(post.title)
+        # print(post.keys())
+        
+            
+        title  = post.title
+        link = post.link
+        nid = post.id
+        published = post.published
+        summary = post.summary
+        # lst = [title,link,nid,published,summary]
+        
+        # news_instance = News.objects.create(title=title,link=link,nid=nid,published=published,summary=summary)
+        viewdict = {
+            'news' : newsdb
+        }
+        
+        # print("---------> News Details <----------- ")
+        # print("Title :",post.title)
+        # print("title_detail:", post.title_detail)
+        # print("links : ",post.links)
+        # print("link : ",post.link)
+        # print("id",post.id)
+        # print("guidislink",post.guidislink)
+        # print("published :",post.published)
+        # print("published_parsed:", post.published_parsed)
+        # print("summary : ",post.summary.text())
+        # print("----------->summary_detail: ",post.summary_detail)
+        # print("source",post.source)
+          
+       
+
+    
+    return render(request,'newsbox/index.html',viewdict)
+    
